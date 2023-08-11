822ea18371d3c9caa2a90b18a5f6c57f5976b1dd
Adam Slay <aslay@mesonet.org>
2023-08-11 08:50:59 -0500

    refactor and clarify shapefile processing

--- src/shapefile_utils.py
+++ src/shapefile_utils.py
@@ -13,31 +13,34 @@ def load_shapefile(file_path):
 def calculate_scaling(shapes):
     """Computes the scaling factors based on provided shapes."""
 
-    all_points = []
+    all_points = collect_all_points(shapes)
 
-    for shp in shapes:
-        if shp.geom_type == 'MultiPolygon':
-            for polygon in shp.geoms:
-                all_points.extend(polygon.exterior.coords)
-        else:
-            all_points.extend(shp.exterior.coords)
-
+    # find the min and max values for x and y through all points
     min_lon = min([x for x, y in all_points])
     max_lon = max([x for x, y in all_points])
     min_lat = min([y for x, y in all_points])
     max_lat = max([y for x, y in all_points])
 
+    # calculate the scaling factor for xax and yax based on the drawing area and the min/max coords
     scale_x = DRAW_WIDTH / (max_lon - min_lon)
     scale_y = DRAW_HEIGHT / (max_lat - min_lat)
 
-    return min(scale_x, scale_y) * 0.95, min_lon, min_lat
+    # find the smaller scaling factor to make the map fit inside the drawing area without being distorted
+    final_scale = min(scale_x, scale_y)
+
+    # scale the map down a bit to make sure it fits inside the drawing area without being cut off
+    final_scale *= 0.95
+
+    return final_scale, min_lon, min_lat
 
 
 def collect_all_points(shapes):
     """Collect all points from the shapes."""
     all_points = []
+
     for shp in shapes:
         if shp.geom_type == 'MultiPolygon':
+            # If the shape is a polygon within a polygon like an island or similar, we need to iterate over each polygon
             for polygon in shp.geoms:
                 all_points.extend(polygon.exterior.coords)
         else:
